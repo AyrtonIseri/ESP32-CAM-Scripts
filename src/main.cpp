3ff5d8078ea70c321901d75486bf8b7568210ac6
Ayrton Iseri <ayrtoniseri@hotmail.com>
2023-05-23 09:31:28 -0300

    Implemented full aws communication. Only S3 POST request debugging is missing

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,12 @@
 #include <HTTPClient.h>
 #include "esp_camera.h"
 
-
+#define OBJECT_FILENAME "photo.jpeg"
 #define MQTT_BUF_SIZE 7168
 #define SLEEP_TIME 10000
 
 String AP_SSID = "AutoConnectAP";
 String AP_PASS = "password";
-
 
 // CAMERA_MODEL_AI_THINKER
 #define PWDN_GPIO_NUM     32
@@ -35,11 +34,12 @@ String AP_PASS = "password";
 #define PCLK_GPIO_NUM     22
 
 WiFiClientSecure net = WiFiClientSecure();
-WiFiClientSecure wifiClient;
 MQTTClient client = MQTTClient(MQTT_BUF_SIZE);
 
 bool RECEIVED_POST_URL = false;
 unsigned long millisNow = -SLEEP_TIME;
+
+camera_fb_t * fb;
 
 void connectWifi() {
   ESP_WiFiManager ESP_wifiManager;
@@ -49,34 +49,32 @@ void connectWifi() {
     Serial.println(F("WiFi connected...yeey :)"));
 }
 
-void publishURLRequest() {
-  StaticJsonDocument<200> doc;
-  doc["time"] = millis();
-  doc["client"] = CLIENT;
-  doc["topic"] = AWS_IOT_SUBSCRIBE_TOPIC;
-  doc["key"] = "photo";
+void uploadFileToS3(String uploadUrl) {
+  HTTPClient http;
+  http.begin(net, uploadUrl);
 
-  char jsonBuffer[512];
-  serializeJson(doc, jsonBuffer);
+  Serial.print("payload size: ");
+  Serial.print(fb->len);
+  Serial.println(".");
 
-  Serial.print("publishing on ");
-  Serial.print(AWS_IOT_PUBLISH_TOPIC);
-  Serial.print(" message: ");
-  Serial.println(jsonBuffer);
-
-  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+  int result = http.POST(fb->buf, fb->len);
+  Serial.print("The result from the post operation was: ");
+  Serial.print(result);
+  Serial.println(".");
+  
+  http.end();
 }
 
 void messageHandler(String &topic, String &payload) {
   RECEIVED_POST_URL = true;
-  Serial.println(sizeof(payload));
-  Serial.println("incoming: " + topic + " - " + payload);
+  Serial.println("Credential Received");
 
-  StaticJsonDocument<200> doc;
-  deserializeJson(doc, payload);
-  // Serial.println(payload);
-  //  const char* message = doc["message"];
-  //  Serial.println(message);
+  StaticJsonDocument<200> url;
+  deserializeJson(url, payload);
+  
+  uploadFileToS3(url["url"]);
+
+  esp_camera_fb_return(fb);
 }
 
 void connectAWS() {  
@@ -102,34 +100,74 @@ void connectAWS() {
   // Subscribe to a topic
   client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
 
-  Serial.println("\nAWS IoT Connected!");
+  Serial.println("AWS IoT Connected!");
 }
 
-void deep_sleep() {
-  delay(SLEEP_TIME);
+void publishURLRequest() {
+  StaticJsonDocument<200> doc;
+  doc["time"] = millis();
+  doc["client"] = CLIENT;
+  doc["topic"] = AWS_IOT_SUBSCRIBE_TOPIC;
+  doc["key"] = OBJECT_FILENAME;
+
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer);
+
+  Serial.print("message being send: ");
+  Serial.println(jsonBuffer);
+
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
 }
 
-void setHTTP() {
-  const char* bucketName = "supin-nema";
-  const char* objectKey = "photo";
-  const char* uploadUrl = "https://supin-nema.s3.amazonaws.com/";
-  const char* contentType = "image/jpg";
-  const char* accessKeyId = "ASIA2E3752DQNPQIBCUQ";
-  const char* securityToken = "IQoJb3JpZ2luX2VjEIH...";
-  const char* policy = "eyJleHBpcmF0aW9u...";
-  const char* signature = "BGsKhBmK/3/8cHqqw2fyUcCUxZ8=";
+void configCamera() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
 
-  HTTPClient http;
-  http.begin(wifiClient, uploadUrl);
-  http.addHeader("ContentType", contentType);
-  http.addHeader("x-amz-security-token", securityToken);
-  http.addHeader("AWSAccessKeyId", accessKeyId);
+  // init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 5;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+  sensor_t * s = esp_camera_sensor_get();
 }
 
 void setup() {
   Serial.begin(115200);
   connectWifi();
   connectAWS();
+  configCamera();
 }
 
 void loop() {
@@ -138,6 +176,14 @@ void loop() {
       connectAWS();
 
     if (millis() > millisNow + SLEEP_TIME) {
+      fb = esp_camera_fb_get();
+
+      if (!fb) {
+        Serial.println("Camera capture failed");
+        delay(1000);
+        return;
+      }
+
       publishURLRequest();
 
       while (!RECEIVED_POST_URL)
@@ -153,4 +199,8 @@ void loop() {
   }
   client.loop();
   // deep_sleep();
+}
+
+void deep_sleep() {
+  delay(SLEEP_TIME);
 }
